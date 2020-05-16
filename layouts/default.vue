7033a9b7b41d8254c1629e3869d102fc547d2f6c
Alexandre Oliveira <rockytvbr@gmail.com>
2020-05-16 00:27:45 -0300

    Add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+    <section>
+        <div class="container">
+            <b-navbar type='is-primary'>
+                <template slot="brand">
+                    <b-navbar-item>BeaufyDock</b-navbar-item>
+                </template>
+                <template slot="start">
+                    <b-navbar-item href="#">
+                        Home
+                    </b-navbar-item>
+                    <b-navbar-item href="#">
+                        Browse
+                    </b-navbar-item>
+                </template>
+                <template slot="end">
+                    <b-navbar-item tag="div">
+                        <div class="buttons">
+                            <a class="button is-light">
+                                <strong>Register</strong>
+                            </a>
+                            <a class="button is-dark">
+                                Log in
+                            </a>
+                        </div>
+                    </b-navbar-item>
+                </template>
+            </b-navbar>
+            <br>
+            <Nuxt />
+        </div>
+    </section>
+</template>
