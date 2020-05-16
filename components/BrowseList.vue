8306d7cbbc28fd70ca044aec83809e26d8afa261
Alexandre Oliveira <rockytvbr@gmail.com>
2020-05-16 20:19:57 -0300

    Break /browse down into components

--- components/BrowseList.vue
+++ components/BrowseList.vue
@@ -1,34 +1,23 @@
 <template>
     <div class="columns is-multiline">
         <div class="column is-4" v-for="mod in mods">
-            <nuxt-link :to="mod.url">
-                <div class="card">
-                    <div class="card-image">
-                        <figure class="image is-4by3">
-                            <img :src="'https://spacedock.info' + mod.background" alt="Placeholder image">
-                        </figure>
-                    </div>
-                    <header class="card-header">
-                        <p class="card-header-title is-centered">
-                            {{ mod.name }}
-                        </p>
-                    </header>
-                    <div class="card-content">
-                        <p class="subtitle is-6 is-centered">
-                            {{ mod.short_description }}
-                        </p>
-                    </div>
-                </div>
-            </nuxt-link>
+            <ListEntry :mod="mod" />
         </div>
     </div>
 </template>
 
 <script>
+import BrowseListEntry from './browse-list/ListEntry'
+
 export default {
     props: {
-        mods: Array
+        mods: Array,
     },
-    name: 'BrowseList'
+
+    name: 'BrowseList',
+
+    components: {
+        'ListEntry': BrowseListEntry
+    }
 }
 </script>
---
+++ components/browse-list/ListEntry.vue
@@ -0,0 +1,31 @@
+<template>
+    <nuxt-link :to="mod.url">
+        <div class="card">
+            <div class="card-image">
+                <figure class="image is-4by3">
+                    <img :src="'https://spacedock.info' + mod.background" alt="Placeholder image">
+                </figure>
+            </div>
+            <header class="card-header">
+                <p class="card-header-title is-centered">
+                    {{ mod.name }}
+                </p>
+            </header>
+            <div class="card-content">
+                <p class="subtitle is-6 is-centered">
+                    {{ mod.short_description }}
+                </p>
+            </div>
+        </div>
+        </nuxt-link>
+</template>
+
+<script>
+export default {
+    props: {
+        mod: Object
+    },
+
+    name: 'BrowseListEntry'
+}
+</script>
