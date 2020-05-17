136bd4b4286005c76dada71e98edc0fa5145f352
Alexandre Oliveira <rockytvbr@gmail.com>
2020-05-17 03:57:43 -0300

    Add a space between changelog entries

--- components/mod-info/Changelog.vue
+++ components/mod-info/Changelog.vue
@@ -1,30 +1,33 @@
 <template>
     <div>
-        <div class="card" v-for="version in versions">
-            <div class="card-content">
-                <div class="media-content">
-                    <p>
-                        <span class="title is-4">
-                            {{ version.friendly_version }}
-                        </span> 
-                        <span class="subtitle">
-                            for Kerbal Space Program {{ version.game_version }}
-                        </span>
-                    </p>
+        <div v-for="version in versions">
+            <div class="card">
+                <div class="card-content">
+                    <div class="media-content">
+                        <p>
+                            <span class="title is-4">
+                                {{ version.friendly_version }}
+                            </span>
+                            <span class="subtitle">
+                                for Kerbal Space Program {{ version.game_version }}
+                            </span>
+                        </p>
 
-                    <p class="subtitle is-6">
-                        Uploaded on {{ version.created | format_date }}
-                    </p>
-                </div>
-                <br>
-                <div class="content">
-                    <p v-if="version.changelog" v-html="version.changelog"></p>
-                    <p v-else><i>No changelog provided</i></p>
-                </div>
-                <div class="card-footer">
-                    <a href="#" class="card-footer-item"><b-icon icon="download" />Download</a>
+                        <p class="subtitle is-6">
+                            Uploaded on {{ version.created | format_date }}
+                        </p>
+                    </div>
+                    <br>
+                    <div class="content">
+                        <p v-if="version.changelog" v-html="version.changelog"></p>
+                        <p v-else><i>No changelog provided</i></p>
+                    </div>
+                    <div class="card-footer">
+                        <a href="#" class="card-footer-item"><b-icon icon="download" />Download</a>
+                    </div>
                 </div>
             </div>
+            <br>
         </div>
     </div>
 </template>
